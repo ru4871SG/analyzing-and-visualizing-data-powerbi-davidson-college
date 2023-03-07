5a367d2bda06a60a710bc7a41caeb21e4ead70f5
Ruddy Setiadi Gunawan <89178109+ru4871SG@users.noreply.github.com>
2023-03-07 22:23:45 +0700

    update - finished week 1

---
+++ week_1_wine_r_script.r
@@ -0,0 +1,22 @@
+#need to use setwd() so we can run R script straight in Power BI
+setwd("C:/Users/Ruddy/Documents/powerbi_edx_course/week 1 wrap-up wine")
+
+library(tidyverse)
+
+winemag <- read_csv("winemag-data-130k-v2.csv")
+
+sum_points_country <- winemag %>% group_by(country) %>% summarize(total_points = sum(points))
+sum_points_winery <- winemag %>% group_by(winery) %>% summarize(total_points = sum(points))
+sum_points_region_1 <- winemag %>% group_by(region_1) %>% summarize(total_points = sum(points))
+sum_points_region_2 <- winemag %>% group_by(region_2) %>% summarize(total_points = sum(points))
+
+sum_price_variety <- winemag %>% group_by(variety) %>% summarize(cost = sum(price))
+
+average_points_country <- winemag %>% group_by(country) %>% summarize(average_points = mean(points))
+
+min_points_country <- winemag %>% group_by(country) %>% summarize(min_points = min(points))
+max_points_country <- winemag %>% group_by(country) %>% summarize(max_points = max(points))
+
+dfs <- list(sum_points_country,average_points_country,min_points_country,max_points_country)
+
+points <- dfs %>% reduce(inner_join, by = "country")
